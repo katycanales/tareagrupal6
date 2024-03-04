1e86b1eb9719279a5dd80196d2f5305fc65b95b4
Katy <kmcanales@uth.hn>
2024-03-04 16:42:27 -0600

    Primer comit para repositorio remoto =

---
+++ repositorio/src/main/java/com/mycompany/repositorio/Repositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repositorio;
+
+/**
+ *
+ * @author 50489
+ */
+public class Repositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Guardar el codigo de github");
+    }
+}
